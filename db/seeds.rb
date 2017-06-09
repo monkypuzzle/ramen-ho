cd4dbf4a5614f1b232dc2b645a1d526afd5e9008
Mendel Dinh <mdinh6@gmail.com>
2017-06-09 12:12:05 -0700

    base algorithm

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+5.times do
+  Waittime.create(party_size: 2, seated: false)
+end
