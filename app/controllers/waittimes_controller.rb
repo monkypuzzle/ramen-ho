98d007c69fc7917c64985cb97107fcefb73ecf59
Quyen Tran <email": null>
2017-06-09 15:53:33 -0700

    Add twilio phone number as env variable

--- app/controllers/waittimes_controller.rb
+++ app/controllers/waittimes_controller.rb
@@ -10,7 +10,7 @@ class WaittimesController < ApplicationController
     waittime = Waittime.new(party_size: params["customer"]["partySize"], restaurant_id: 1)
     customer = {name: params["customer"]["name"], phone: params["customer"]["phone"], party_size: waittime.party_size}
     if waittime.save
-        # send_confirmation_sms(customer[:phone])
+        send_confirmation_sms(customer[:phone])
         render partial: "/waittimes/create.html.erb", locals: { customer: customer, waittime: waittime }, layout: false
     else
       # print error messages?
@@ -18,7 +18,7 @@ class WaittimesController < ApplicationController
   end
 
   def send_notice
-    send_ready_sms(params[:phone_number])
+    # send_ready_sms(params[:phone_number])
   end
 
   def waittime_params
