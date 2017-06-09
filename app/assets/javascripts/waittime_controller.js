03d6060eccca38002970adbb979c273a010742e0
monkypuzzle <paul.newsam@gmail.com>
2017-06-08 19:59:49 -0700

    User can remove waittime item from list

--- app/assets/javascripts/waittime_controller.js
+++ app/assets/javascripts/waittime_controller.js
@@ -1,16 +1,23 @@
 $(document).ready(function(){
 
-  $("waitlist").on("submit", "waittime-seat-form", function(){
-    var $form = $(this)
+  $(".waitlist").on("submit", ".waittime-seat-form", function(event){
+    event.preventDefault()
+    var $form = $(this);
+    var $chosenWaittimeItem = $(this).closest(".waittime-item");
+    var waittimeId = $chosenWaittimeItem.attr("id").replace("waittime-","");
+    console.log(waittimeId)
+    var data = {'waittime': {'id': waittimeId}};
     // UPDATE Waittime object in the database (set seated to true)
     $.ajax({
-      method: "PUT",
+      method: "PATCH",
       url: $form.attr("action"),
-      
+      dataType: "json",
+      data: data
     })
     .done(function(response){
       // When done, remove the <li> from the list
-    })
-  })
+      $chosenWaittimeItem.remove();
+    });
+  });
 
 });
