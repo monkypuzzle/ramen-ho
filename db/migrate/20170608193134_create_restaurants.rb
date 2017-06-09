13bf7da518b202cac4ea7d286adfd673e8d1f4a4
monkypuzzle <paul.newsam@gmail.com>
2017-06-08 18:41:55 -0700

    Adds constraints to migrations, associations, some seed data, routes

--- db/migrate/20170608193134_create_restaurants.rb
+++ db/migrate/20170608193134_create_restaurants.rb
@@ -1,12 +1,12 @@
 class CreateRestaurants < ActiveRecord::Migration
   def change
     create_table :restaurants do |t|
-      t.string :name
+      t.string :name, null: false
       t.integer :number_of_seats
       t.integer :crowdsourced_waittime
-      t.jsonb :operating_hours
-      t.string :address
-      t.string :phone
+      t.jsonb :operating_hours, null: false
+      t.string :address, null: false
+      t.string :phone, null: false
 
       t.timestamps null: false
     end
