db5eb5bc82687e5f048d0048419cc3d9a5decedb
Lisa D'Asaro <email": null>
2017-06-09 18:55:40 -0700

    Dirty restaurant code logic added.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -26,10 +26,13 @@ class Restaurant < ActiveRecord::Base
       elsif current_mil_time > self.operating_hours[current_day]["dinner"][1]
         tomorrow = (Date.today + 1).strftime("%A").downcase
         day_after_tmrw = (Date.today + 2).strftime("%A").downcase
-        self.operating_hours[tomorrow]["lunch"][0]
-        self.operating_hours[tomorrow]["dinner"][0]
-        self.operating_hours[day_after_tmrw]["lunch"][0]
-        self.operating_hours[day_after_tmrw]["dinner"][0]
+        if self.operating_hours[tomorrow]["lunch"][0] == "0"
+          if self.operating_hours[tomorrow]["dinner"][0] == "0"
+            if self.operating_hours[day_after_tmrw]["lunch"][0] == "0"
+              self.operating_hours[day_after_tmrw]["dinner"][0]
+            end
+          end
+        end
       end
     else
       "Unknown"
