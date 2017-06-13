a64620937ed0faf79cfe755f3a2e1bf907a055b3
Mendel Dinh <mdinh6@gmail.com>
2017-06-13 11:13:39 -0700

    basic partial

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,5 +1,40 @@
 module RestaurantsHelper
 
-  def hours_parse
+  def hours_parse(hours_in_json)
+    arr = hours_in_json.map{|day| day}
+    days = []
+    lunch_hours = []
+    dinner_hours =[]
+    i = 0
+    until i == arr.length - 1
+      days << arr[i][0]
+      lunch_hours << arr[i][1]["lunch"]
+      dinner_hours << arr[i][1]["dinner"]
+      i += 1
+    end
+
+  lunch_hours.flatten
+  dinner_hours.flatten
+  info_hash = {day_list: days, lunch: lunch_hours, dinner: dinner_hours}
   end
+
+
+
+  def hours_show(hours)
+    hash = hours_parse(hours)
+    display = []
+    i = 0
+    until i == hash[:day_list].length - 1
+      if hash[:lunch][i][1] == "0" && hash[:dinner][i][0]
+        display << hash[:day_list][i].capitalize  + ":  Open From: " +  hash[:lunch][i][0] + " -  " + hash[:dinner][i][1]
+      else
+        display << hash[:day_list][i].capitalize  + ":  Lunch: " +  hash[:lunch][i][0] + " - " + hash[:lunch][i][1] + " || Dinner: " +  hash[:dinner][i][0] + " - " + hash[:dinner][i][1]
+      end
+      i += 1
+    end
+
+    display
+  end
+
+
 end
