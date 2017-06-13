8e63596fe793dfde8f164388f7c7788a30c234d8
Mendel Dinh <mdinh6@gmail.com>
2017-06-13 12:31:01 -0700

    standard time done, operating hours done

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -1,5 +1,28 @@
 module RestaurantsHelper
 
+  def standard_time(string)
+    p string
+    time = string.gsub(/[:]/, "")
+    if time.include?("12")
+      result = time.gsub(/^\d{2}/, '12:')
+    elsif string == "0"
+      result = "-CLOSED-"
+    elsif time > ("1200")
+      first_two = time.match(/^\D*(\d)\D*(\d)/).captures.join
+      new_hour = first_two.to_i - 12
+      result = time.gsub(/^\d{2}/, new_hour.to_s + ':')
+      result = result + " PM"
+    else
+      p time
+      p time.match(/^\D*(\d)\D*(\d)/)
+      first_two = time.match(/^\D*(\d)\D*(\d)/).captures.join
+      result = time.gsub(/^\d{2}/, first_two + ':')
+      result = result + " AM"
+    end
+
+    result
+  end
+
   def hours_parse(hours_in_json)
     arr = hours_in_json.map{|day| day}
     days = []
@@ -23,18 +46,18 @@ module RestaurantsHelper
   def hours_show(hours)
     hash = hours_parse(hours)
     display = []
+    # puts hash
     i = 0
     until i == hash[:day_list].length - 1
-      if hash[:lunch][i][1] == "0" && hash[:dinner][i][0]
-        display << hash[:day_list][i].capitalize  + ":  Open From: " +  hash[:lunch][i][0] + " -  " + hash[:dinner][i][1]
+      if hash[:lunch][i][1] == "0" && hash[:dinner][i][0] == "0"
+        display << hash[:day_list][i].capitalize  + ":  Open From: " +  standard_time(hash[:lunch][i][0]) + " -  " + standard_time(hash[:dinner][i][1])
       else
-        display << hash[:day_list][i].capitalize  + ":  Lunch: " +  hash[:lunch][i][0] + " - " + hash[:lunch][i][1] + " || Dinner: " +  hash[:dinner][i][0] + " - " + hash[:dinner][i][1]
+        display << hash[:day_list][i].capitalize  +  ":  Lunch: " +  standard_time(hash[:lunch][i][0]) + " - " + standard_time(hash[:lunch][i][1]) + " || Dinner: " +  standard_time(hash[:dinner][i][0]) + " - " + standard_time(hash[:dinner][i][1])
       end
       i += 1
     end
 
     display
   end
-
 
 end
